025c8607b3e355b8a497cf02f97e253a4a18e548
urmum6959 <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>
2022-10-19 19:57:35 +0100

    men

--- form.php
+++ form.php
@@ -2,10 +2,10 @@
 
 if(isset($_POST['submitquestion'])) {
     $question = $_POST['question'];
-    $subject = $_POST['uwu'];
+    $subject = $_POST["uwu"];
     
-    $emailaddress = $_POST['skillama@outlook.com'];
-    $emailheaders = $_POST['From: Anonymous'];
+    $emailaddress = $_POST["skillama@outlook.com"];
+    $emailheaders = $_POST["From: Anonymous"];
     $emailtext = $_POST["You've got mail!\n".$question];
 
     mail($emailaddress, $subject, $emailtext, $emailheaders);
