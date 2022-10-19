e3024cc7d958fb0e83e5ab577d170e9efc3e5fa9
urmum6959 <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>
2022-10-19 19:42:52 +0100

    email test

---
+++ form.php
@@ -0,0 +1,13 @@
+<?php
+
+if(isset($_POST['submitquestion'])) {
+    $question = $_POST['question'];
+    
+    $subject = "uwu";
+    $emailaddress = "skillama@outlook.com";
+    $emailheaders = "From: Anonymous";
+    $emailtext = "You've got mail!\n\n\n".$question;
+
+    mail($emailaddress, $subject, $emailtext, $emailheaders);
+    header("Location: index?mailsend");
+}
