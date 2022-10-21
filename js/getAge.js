0a7538fda60ca0c298dc5cb8e1a7669089dc9dc8
urmum6959 <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>
2022-10-21 19:18:39 +0100

    design overhaul v1

---
+++ js/getAge.js
@@ -0,0 +1,15 @@
+function calculateAge(dobString) {
+var dob = new Date(dobString);
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
+var birthdayThisYear = new Date(currentYear, dob.getMonth(), dob.getDate());
+var age = currentYear - dob.getFullYear();
+
+if(birthdayThisYear > currentDate) {
+    age--;
+}
+
+return age;
+}
+myAge = calculateAge('2008-10-11')
+document.write(myAge);
