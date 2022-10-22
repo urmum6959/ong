7ee2e09dfd3f28b8f553e8bc0736ee6a84e708d9
urmum6959 <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>
2022-10-22 14:17:18 +0100

    navbar

---
+++ js/navburger.js
@@ -0,0 +1,8 @@
+const tglBtn = document.getElementsByClassName('tglnv')[0]
+const nvLnks = document.getElementsByClassName('navlnks')[0]
+
+window.addEventListener('load', () =>{
+    tglBtn.addEventListener('click', () => {
+        nvLnks.classList.toggle('active')
+    })
+})
